a27c042bc0bc85252c49eee923ed5fd1127a66ef
Jovan Dozic <jovandozic@gmail.com>
2023-10-22 23:33:23 +0200

    feat: Implemented basic geocoding and routing. Added examples for setting markers on map, and for creating a route from two addresses (forms from map component will be removed).

--- Explorer/src/app/shared/map/map.component.ts
+++ Explorer/src/app/shared/map/map.component.ts
@@ -1,5 +1,8 @@
 import { Component, AfterViewInit } from '@angular/core';
+import { MapService } from './map.service';
 import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import { environment } from 'src/env/environment';
 
 @Component({
   selector: 'xp-map',
@@ -7,16 +10,20 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements AfterViewInit {
-  private map: any;
 
-  constructor() {}
+  private map: any;
+  searchAddress: string = '';
+  startingAddress: string = '';
+  endingAddress: string = '';
+
+  constructor(private mapService: MapService) { }
 
   private initMap(): void {
     this.map = L.map('map', {
       center: [45.2396, 19.8227],
       zoom: 13,
     });
-
+    
     const tiles = L.tileLayer(
       'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       {
@@ -29,6 +36,8 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
     this.registerOnClick();
   }
+
+
 
   registerOnClick(): void {
     this.map.on('click', (e: any) => {
@@ -45,9 +54,79 @@ export class MapComponent implements AfterViewInit {
   ngAfterViewInit(): void {
     let DefaultIcon = L.icon({
       iconUrl: 'https://unpkg.com/leaflet@1.6.0/dist/images/marker-icon.png',
+      iconAnchor: [13, 41],
     });
+
+    // here
 
     L.Marker.prototype.options.icon = DefaultIcon;
     this.initMap();
+  }
+
+  // Primer sam dao za search funkciju
+  search(searchAddress: string): void {
+    this.mapService.search(searchAddress).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        if (result[0]) {
+          L.marker([result[0].lat, result[0].lon])
+            .addTo(this.map)
+            .bindPopup('Pozdrav iz ' + searchAddress + '!')
+            .openPopup();
+
+          // Metoda nece raditi zbog CORS Policy-ja
+          this.mapService.getElevation(result[0].lat, result[0].lon).subscribe({
+            next: (elevation: any) => {
+              console.log('Nadmorska visina na toj adresi: ' + elevation);
+              alert('Nadmorska visina na toj adresi: ' + elevation);
+            },
+            error: () => { },
+          });
+
+        }
+      },
+      error: () => { },
+    });
+  }
+
+  // Primer sam dao za pretragu rute
+  findRoute(startingAddress: string, endingAddress: string): void {
+    let startLatLng: L.LatLng;
+    let endLatLng: L.LatLng;
+    this.mapService.search(startingAddress).subscribe({
+      next: (result: any) => {
+        console.log(result);
+        if (result[0]) {
+          startLatLng =  L.latLng(result[0].lat, result[0].lon);
+
+          this.mapService.search(endingAddress).subscribe({
+            next: (result: any) => {
+              console.log(result);
+              if (result[0]) {
+                endLatLng =  L.latLng(result[0].lat, result[0].lon);
+                this.setRoute(startLatLng, endLatLng);
+              }
+            },
+            error: () => { },
+          });
+
+        }
+      },
+      error: () => { },
+    });
+  }
+
+  // Dat primer, samo sa parametrima
+  setRoute(startLatLng: L.LatLng, endLatLng: L.LatLng): void {
+    const routeControl = L.Routing.control({
+      waypoints: [startLatLng, endLatLng],
+      router: L.routing.mapbox(environment.mapBoxApiKey, { profile: 'mapbox/walking' })
+    }).addTo(this.map);
+
+    routeControl.on('routesfound', function (e) {
+      var routes = e.routes;
+      var summary = routes[0].summary;
+      alert('Total distance is ' + summary.totalDistance / 1000 + ' km and total time is ' + Math.round(summary.totalTime % 3600 / 60) + ' minutes');
+    });
   }
 }
