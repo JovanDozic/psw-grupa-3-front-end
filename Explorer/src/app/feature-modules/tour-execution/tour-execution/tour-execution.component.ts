f631fcc403782ee0c723417d0f587017832bd388
Marko Košarac <marko_kosarac@hotmail.com>
2023-11-11 15:24:31 +0100

    Small change

--- Explorer/src/app/feature-modules/tour-execution/tour-execution/tour-execution.component.ts
+++ Explorer/src/app/feature-modules/tour-execution/tour-execution/tour-execution.component.ts
@@ -14,18 +14,10 @@ export class TourExecutionComponent implements OnInit {
   tour: Tour = {id: 1,name: "Markova", description: "odlicna",
                   difficult: 2, tags:"nista", status:"draft", price: 3, authorId: 1}
 
-  @Output() points: Point[] = [{id: 1, longitude: 19.83966064452716 , latitude: 45.2517365956994,
-    name:"prva", description:"nista", picture:"nista", tourId: 1}]
-
   ngOnInit(): void {
     
     };
-/*
-    addPoint(): void{
-      this.points.push({id: 1, longitude: 19.83966064452716 , latitude: 45.2517365956994,
-        name:"prva", description:"nista", picture:"nista", tourId: 1})
-    }
-*/
+
     handleUpdatedPosition(updatedPosition: Position) {
       this.position = updatedPosition;
       console.log(this.position)
