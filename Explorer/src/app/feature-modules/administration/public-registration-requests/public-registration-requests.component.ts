d0ea235fc0b9da433f2858199f36b9870847f896
biljanamijic26 <bilja2610@gmail.com>
2023-11-12 19:07:42 +0100

    Approving and rejecting requests implemented

--- Explorer/src/app/feature-modules/administration/public-registration-requests/public-registration-requests.component.ts
+++ Explorer/src/app/feature-modules/administration/public-registration-requests/public-registration-requests.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { AdministrationService } from '../administration.service';
 import { TourAuthoringService } from '../../tour-authoring/tour-authoring.service';
 import { PublicRegistrationRequest } from '../model/public-registration-request.model';
-import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { PagedResults } from 'src/app/shared/model/paged-results.model';
 import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
@@ -16,7 +14,6 @@ export class PublicRegistrationRequestsComponent implements OnInit {
   constructor(private service: AdministrationService, private tourService: TourAuthoringService ) { }
 
   registrationRequests: PublicRegistrationRequest[] = [];
-  isRejectModalVisible: boolean = false;
   selectedRequest: PublicRegistrationRequest;
 
   rejectionForm = new FormGroup({
@@ -36,61 +33,43 @@ export class PublicRegistrationRequestsComponent implements OnInit {
       }
     })
   }
+  
+  selectRequest(request: PublicRegistrationRequest) {
+       this.selectedRequest = request;
+    }   
 
-  openRejectModal(request: PublicRegistrationRequest) {
-    this.isRejectModalVisible = true;
-    this.selectedRequest = request;
-  }   
+   confirmReject() {
+     const foundRequest = this.registrationRequests.find(r => r === this.selectedRequest);
+     if(foundRequest){
+       foundRequest.status = 'Rejected';
+       foundRequest.comment = this.rejectionForm.value.rejectionComment || "";
 
-  closeRejectModal() {
-    this.isRejectModalVisible = false;
-  }
+       this.service.updatePublicRegistrationRequest(foundRequest).subscribe({
+         next: () => {}
+       });
+     }
+     this.registrationRequests = this.registrationRequests.filter(r => r !== foundRequest);
 
-  confirmReject() {
+   }
+
+   confirmApprove() : void {
     const foundRequest = this.registrationRequests.find(r => r === this.selectedRequest);
-    if(foundRequest){
-      foundRequest.status = 'Rejected';
-      foundRequest.comment = this.rejectionForm.value.rejectionComment || "";
-
-      this.service.updatePublicRegistrationRequest(foundRequest).subscribe({
-        next: () => {}
-      });
-    }
-    this.registrationRequests = this.registrationRequests.filter(r => r !== foundRequest);
-
-    this.closeRejectModal();
-  }
-
-  approveRequest(request: PublicRegistrationRequest) : void {
-    const foundRequest = this.registrationRequests.find(r => r === request);
     if(foundRequest){
       foundRequest.status = 'Approved';
 
       this.service.updatePublicRegistrationRequest(foundRequest).subscribe({
         next: () => {
-           if(foundRequest.objectId !== -1)
-           {
-             this.tourService.setPublicObject(foundRequest.objectId).subscribe({
-               next: () => {}
-             })
-           }
+          if(foundRequest.objectId !== -1)
+          {
+            this.tourService.setPublicObject(foundRequest.objectId).subscribe({
+              next: () => {}
+            })
+          }
         }
       });
     }
     this.registrationRequests = this.registrationRequests.filter(r => r !== foundRequest);
-  }
-
-  rejectRequest(request: PublicRegistrationRequest) : void {
-    const foundRequest = this.registrationRequests.find(r => r === request);
-    if(foundRequest){
-      foundRequest.status = 'Rejected';
-
-      this.service.updatePublicRegistrationRequest(foundRequest).subscribe({
-        next: () => {}
-      });
-    }
-    this.registrationRequests = this.registrationRequests.filter(r => r !== foundRequest);
-  }
+   }
 
 }
 
