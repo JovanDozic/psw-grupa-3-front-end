c381bfcff03fef120b4d39ee4e096f0050b251e9
Luka Milanko <milankoluka01@gmail.com>
2023-11-15 03:25:59 +0100

    Committing changes before reset

---
+++ Explorer/src/app/feature-modules/marketplace/show-tour/show-tour.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Tour } from '../../tour-authoring/model/tour.model';
+import { MatGridListModule } from '@angular/material/grid-list';
+
+@Component({
+  selector: 'xp-show-tour',
+  templateUrl: './show-tour.component.html',
+  styleUrls: ['./show-tour.component.css']
+})
+export class ShowTourComponent {
+  tour: Tour = {
+    name: 'Tura u planinama',
+    description: 'Istraživanje prelepih planinskih predela.',
+    difficult: 4,
+    tags: 'Planinarenje, Priroda',
+    status: 'Aktivna',
+    price: 120.00,
+    authorId: 1
+  };
+}
