7b33611233f3052c89cc17b38e6a8998fed8d8b7
Luka Milanko <milankoluka01@gmail.com>
2023-12-06 20:20:08 +0100

    adding bundle to cart ended

--- Explorer/src/app/feature-modules/marketplace/model/order-item.model.ts
+++ Explorer/src/app/feature-modules/marketplace/model/order-item.model.ts
@@ -1,6 +1,12 @@
 export interface OrderItem {
-    idTour: number;
+    idType: number;
     name: string;
     price: number;
     image: string;
+    type: OrderItemType;
+}
+
+export enum OrderItemType {
+    bundle = 'Bundle',
+    singleTour = 'SingleTour'
 }
