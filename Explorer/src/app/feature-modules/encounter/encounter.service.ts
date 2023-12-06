144b320fafed7e9891667714142d98aa36d476cd
biljanamijic26 <bilja2610@gmail.com>
2023-12-06 22:51:15 +0100

    feat: Creating hidden location encounter

--- Explorer/src/app/feature-modules/encounter/encounter.service.ts
+++ Explorer/src/app/feature-modules/encounter/encounter.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Encounter } from './model/encounter.model';
+import { environment } from 'src/env/environment';
+import { Observable } from 'rxjs';
+import { HiddenEncounter } from './model/hidden-encounter.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EncounterService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  addHiddenEncounter(encounter: HiddenEncounter): Observable<HiddenEncounter>{
+    return this.http.post<HiddenEncounter>(environment.apiHost + `hidden-encounters`, encounter);
+  }
 }
