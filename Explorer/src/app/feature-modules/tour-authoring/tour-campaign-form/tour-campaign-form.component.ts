69fa6d2c9c7196582d06f7b43182e5f9ddd08bd7
AnjaMaksimovic <maksimovicanja11@gmail.com>
2023-12-07 01:07:38 +0100

    Tour images fixed. Token model changed. MyOwn tour bool added.

--- Explorer/src/app/feature-modules/tour-authoring/tour-campaign-form/tour-campaign-form.component.ts
+++ Explorer/src/app/feature-modules/tour-authoring/tour-campaign-form/tour-campaign-form.component.ts
@@ -37,7 +37,7 @@ export class TourCampaignFormComponent implements OnInit {
       this.availableTours = pagedResults.results;
       this.tourExeService.getTouristTokens(this.user?.id!).subscribe((tokens) => {
         this.availableTours = this.availableTours.filter((tour) => {
-          return tokens.find((token) => token.tourId === tour.id) !== undefined;
+          return tokens.find((token) => token.typeId === tour.id) !== undefined;
         });
         this.sortTours();
       });
