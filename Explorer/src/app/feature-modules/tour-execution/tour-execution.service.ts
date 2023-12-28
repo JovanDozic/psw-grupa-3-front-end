d9914ae2592d09954759ff52b8fd9fe8b337a7be
Jovana Ristic <jovana012002@gmail.com>
2023-12-28 15:35:02 +0100

    feat: tour-statistics

--- Explorer/src/app/feature-modules/tour-execution/tour-execution.service.ts
+++ Explorer/src/app/feature-modules/tour-execution/tour-execution.service.ts
@@ -15,6 +15,30 @@ import {PagedResults} from "../../shared/model/paged-results.model";
   providedIn: 'root'
 })
 export class TourExecutionService {
+  getAllActiveTours(): Observable<number> {
+    return this.http.get<number>(environment.apiHost + 'author/tourExecution/getAllActiveToursCount');
+  }
+  getAllCompletedTours(): Observable<number> {
+    return this.http.get<number>(environment.apiHost + 'author/tourExecution/getAllCompletedToursCount');
+  }
+  getAllPurchasedTours(user: User): Observable<number> {
+    return this.http.get<number>(environment.apiHost + 'tourist/tourPurchaseToken/purchasedTourCount/'+ user.id);
+  }
+  getAllAuthorsTours(user: User): Observable<Tour[]> {
+    return this.http.get<Tour[]>(environment.apiHost + 'author/tour/getAllAuthorsTours/'+ user.id);
+  }
+  getCompletedTourCount(tourId:number): Observable<number> {
+    return this.http.get<number>(environment.apiHost + 'author/tourExecution/getCompletedTourCount/'+ tourId);
+  }
+  getActiveTourCount(tourId:number): Observable<number> {
+    return this.http.get<number>(environment.apiHost + 'author/tourExecution/getActiveTourCount/'+ tourId);
+  }
+  getPurchaseCount(user: User, tourId:number): Observable<number> {
+    const url = `${environment.apiHost}tourist/tourPurchaseToken/purchaseCount/${user.id}/${tourId}`;
+
+    return this.http.get<number>(url);
+  }
+  
   user: BehaviorSubject<User>;
   constructor(private http: HttpClient, private authService: AuthService) {
     this.user = authService.user$;
